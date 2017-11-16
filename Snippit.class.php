07007db08c4d4c773b85c352d4f2dc1bd8199915
Peter 4A5A <498883@edu.rocmn.nl>
2017-11-16 13:24:08 +0100

    Added a extends class

--- Snippit.class.php
+++ Snippit.class.php
@@ -4,15 +4,12 @@
     private $gameName;
     private $character;
 
-    /**
-     * Sets the class obj when the class is declared
-     */
-    public function __construct($gameName) {
+    protected function setGame($gameName) {
       $this->gameName = $gameName;
     }
 
-    public function setCharacter($character) {
-      $this->character = $character;
+    protected function setCharacter($char) {
+      $this->character = $char;
     }
 
     public function getHTML() {
@@ -26,7 +23,42 @@
     public function __toString() {
       return("<span class='{$this->gameName} {$this->character}'></span>");
     }
+
+    public function walk() {
+      return('walk');
+    }
   }
+
+  class Mario extends Snippit {
+
+    public function __construct() {
+      $this->setGame('mario-kart-character');
+      $this->setCharacter('mario');
+    }
+
+    public function jump() {
+      return('Jump');
+    }
+  }
+
+  class Lugui extends Snippit{
+    public function __construct() {
+      $this->setGame('mario-kart-character');
+      $this->setCharacter('lugiu');
+    }
+
+    public function swim() {
+      return('swim');
+    }
+  }
+  $Mario = new Mario();
+  echo $Mario->walk();
+  echo "<br>";
+  echo $Mario->jump();
+  echo "<br>";
+  echo "<br>";
+  $Lugui = new Lugui();
+  echo "Lui:" . $Lugui->swim();
 
 
 ?>
